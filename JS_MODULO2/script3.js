a7f294cd92cf7caef1dbb9f3c034d71f0e210b4a
DaviBJava <davibbraga864@gmail.com>
2025-12-08 11:03:56 -0300

    att

--- JS_MODULO2/script3.js
+++ JS_MODULO2/script3.js
@@ -3,11 +3,6 @@ let titulo = document.querySelector("#titulo");
 titulo.innerText = "Minha Página Manipulada!";
 console.log(titulo);
 
-// 2. Selecione a descrição e mude seu texto
-let descricao = document.querySelector("#descricao");
-descricao.innerText = "Veja como o JavaScript pode mudar tudo!";
-console.log(descricao);
-
 // 3. Selecione a imagem e mude o 'src' e o 'alt'
 let imagem = document.querySelector("#imagem-principal");
 imagem.src = "https://placehold.co/150x150/0000FF/FFFFFF?text=Azul"; // Agora um quadrado azul
@@ -23,3 +18,19 @@ listaContainer.innerHTML = `
     <li>Ovos</li>
   </ul>
 `;
+
+let aviso = document.querySelector("#aviso");
+
+if (aviso.classList.contains("destaque")) {
+  console.log("O aviso já está com o estilo!");
+} else {
+  console.log("O aviso está sem o estilo");
+  aviso.classList.add("destaque");
+}
+
+if (aviso.classList.contains("destaque")) {
+  console.log("O aviso já está com o estilo!");
+} else {
+  console.log("O aviso está sem o estilo");
+  aviso.classList.add("destaque");
+}
